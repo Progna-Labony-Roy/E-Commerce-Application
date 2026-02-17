849cc99ddfc6cf2971ae73c9a5e347a8f665f690
Progna_Labony_Roy <labony.progna@gmail.com>
2026-02-17 23:39:39 +0600

    commit

--- script/products.js
+++ script/products.js
@@ -52,29 +52,29 @@ const displayCategoryProduct = (products) => {
       <div class="card bg-base-100 lg:w-96 md:w-80 lg:h-[460px] md:h-[420px] shadow-sm">
         <figure>
           <img class="lg:h-72 md:h-64 md:p-5"
-            src="${product.image}"
-            alt="${product.title}"
+            src="${product?.image}"
+            alt="${product?.title}"
           />
         </figure>
         <div class="card-body">
           <div class="flex justify-between">
             <p class="text-blue-500 bg-blue-50 rounded-full px-2 text-xs">
-              ${product.category}
+              ${product?.category}
             </p>
             <div class="flex">
               <i class="fa-solid fa-star text-yellow-400 pt-1"></i>
               <p class="mx-2">
-                <span class="font-semibold">${product.rating.rate}</span> 
-                (${product.rating.count})
+                <span class="font-semibold">${product?.rating?.rate}</span> 
+                (${product?.rating?.count})
               </p>
             </div>
           </div>
           <div class="ml-2">
             <p class="font-semibold">${product.title}</p>
-            <p class="font-bold">$${product.price}</p>
+            <p class="font-bold">$${product?.price}</p>
           </div>
           <div class="card-actions justify-between">
-            <button class="btn btn-outline w-[48%]">Details</button>
+            <button id="product-details-${product?.category}" class="btn btn-outline w-[48%]">Details</button>
             <button class="btn btn-primary w-[48%]">Add</button>
           </div>
         </div>
